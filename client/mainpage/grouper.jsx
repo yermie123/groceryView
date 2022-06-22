ba38a49ae510f75c0168f3c82a5616225fd93bf0
yermie <yermolovpersonal@gmail.com>
2022-06-22 14:06:36 -0700

    working with react

--- client/mainpage/grouper.jsx
+++ client/mainpage/grouper.jsx
@@ -1,1 +1,20 @@
 import React, { Component } from 'react';
+import Pic from './pic.jsx';
+import Deets from './deets.jsx';
+import Link from './link.jsx';
+
+class Grouper extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div id="info-card">
+                <Pic />
+                <Deets />
+                <Link />
+            </div>
+        );
+    }
+}
