348d7c692df2d75f592c7fb84322887bceadd905
yermie <yermolovpersonal@gmail.com>
2022-09-25 20:29:32 -0700

    Minor frontend touchups

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,24 @@
 import NavBar from "../NavBar/NavBar";
 
+import './AboutStyle.scss';
+
 export default function About(){
     return(
         <div>
             <NavBar />
-            <div>
-                <h2>This is the About Page</h2>
+            <div id="aboutpage">
+                <h2>GroceryView is a tool for visualizing change in prices of common supermarket goods.</h2>
+                <div id='pcontent'>
+                    <p>
+                        Today, many prices drastically change for different goods.
+                        Think of the recent time that baby formula was out of stock nearly everywhere.
+                        While we are still under heavy development, we hope to bring about more information regarding
+                        the change in prices from place to place.
+                    </p>
+                    <p>
+                        We appreciate any donations to support our efforts.
+                    </p>
+                </div>
             </div>
         </div>
     )
--- src/components/MyList/MyList.jsx
+++ src/components/MyList/MyList.jsx
@@ -4,6 +4,9 @@ export default function MyList(){
     return(
         <div>
             <NavBar />
+            <div id="mylistpage">
+                <p>This is where the list content goes depending on what you chose to track</p>
+            </div>
         </div>
     )
 }
